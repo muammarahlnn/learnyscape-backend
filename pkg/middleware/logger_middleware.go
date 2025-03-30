bf8faa5af4557b609da030aeaf4e4029e99dc15b
muammarahlnn <21muammarahlan@gmail.com>
2025-03-31 01:57:05 +0800

    feat: fix pkg module name

--- pkg/middleware/logger_middleware.go
+++ pkg/middleware/logger_middleware.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 	"time"
 
+	"learnyscape-backend/pkg/httperror"
+	"learnyscape-backend/pkg/logger"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"github.com/muammarahlnn/learnyscape-backend/pkg/httperror"
-	"github.com/muammarahlnn/learnyscape-backend/pkg/logger"
 )
 
 func Logger(logger logger.Logger) gin.HandlerFunc {
