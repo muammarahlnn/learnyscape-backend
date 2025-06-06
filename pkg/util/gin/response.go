8348ff11a59a58d657e66b201e8ae0a4e978aa81
muammarahlnn <21muammarahlan@gmail.com>
2025-06-06 11:45:31 +0800

    feat: add gin util

---
+++ pkg/util/gin/response.go
@@ -0,0 +1,28 @@
+package ginutil
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/muammarahlnn/learnyscape-backend/pkg/dto"
+)
+
+func ResponseOK[T any](ctx *gin.Context, data T) {
+	ResponseJSON(ctx, http.StatusOK, data, nil)
+}
+
+func ResponseCreated[T any](ctx *gin.Context, data T) {
+	ResponseJSON(ctx, http.StatusCreated, data, nil)
+}
+
+func ResponsePagination[T any](ctx *gin.Context, data T, paging *dto.PageMetaData) {
+	ResponseJSON(ctx, http.StatusOK, data, paging)
+}
+
+func ResponseNoContent(ctx *gin.Context) {
+	ResponseJSON[any](ctx, http.StatusNoContent, nil, nil)
+}
+
+func ResponseJSON[T any](ctx *gin.Context, statusCode int, data T, paging *dto.PageMetaData) {
+	ctx.JSON(statusCode, dto.WebResponse[T]{Data: data, Paging: paging})
+}
