abcdf876444ee81381d786a484762b19de603c36
muammarahlnn <21muammarahlan@gmail.com>
2025-07-03 21:06:04 +0800

    feat: add redis cluster pkg

--- pkg/database/redis.go
+++ pkg/database/redis.go
@@ -20,6 +20,18 @@ type RedisOptions struct {
 	MaxConnLifetime int
 }
 
+type RedisClusterOptions struct {
+	Addrs           []string
+	Password        string
+	DialTimeout     int
+	ReadTimeout     int
+	WriteTimeout    int
+	MinIdleConn     int
+	MaxIdleConn     int
+	MaxActiveConn   int
+	MaxConnLifetime int
+}
+
 func NewRedis(opt *RedisOptions) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:            opt.Addr,
@@ -38,6 +50,29 @@ func NewRedis(opt *RedisOptions) *redis.Client {
 		log.Fatalf("failed to connect to redis: %v", err)
 	}
 	log.Println("connected to redis:", status)
+
+	return rdb
+}
+
+func NewRedisCluster(opt *RedisClusterOptions) *redis.ClusterClient {
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:           opt.Addrs,
+		Password:        opt.Password,
+		DialTimeout:     time.Duration(opt.DialTimeout) * time.Second,
+		ReadTimeout:     time.Duration(opt.ReadTimeout) * time.Second,
+		WriteTimeout:    time.Duration(opt.WriteTimeout) * time.Second,
+		MinIdleConns:    opt.MinIdleConn,
+		MaxIdleConns:    opt.MaxIdleConn,
+		ConnMaxLifetime: time.Duration(opt.MaxConnLifetime) * time.Minute,
+		MaxActiveConns:  opt.MaxActiveConn,
+	})
+
+	ctx := context.Background()
+	status, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("failed to connect to redis cluster: %v", err)
+	}
+	log.Println("connected to redis cluster:", status)
 
 	return rdb
 }
